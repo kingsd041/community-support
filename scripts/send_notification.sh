91489773e2c97e69664b4bb4f876e8f87954b233
kingsd041 <nicholas_ksd@hotmail.com>
2021-03-03 08:55:27 +0800

    update

--- scripts/send_notification.sh
+++ scripts/send_notification.sh
@@ -6,8 +6,8 @@ keyword='Download the required resources successfully'
 host_data=`date "+%Y-%m-%d %H:%M:%S"`
 host_hostname=`hostname`
 host_ip=`/sbin/ifconfig eth0 | grep -w inet | awk  '{print $2}'`
-mail_tile_failure="[FAILED] Ranher mirror download failed"
-mail_tile_successful="[SUCCESS] Ranher mirror download successful ！！！"
+mail_tile_failure="[FAILED] Rancher mirror download failed"
+mail_tile_successful="[SUCCESS] Rancher mirror download successful ！！！"
 receive_mail="nicholas_ksd@hotmail.com"
 mail_content=`cat $log_dir/$log_file $receive_mail | grep -v "already exists"`
 
