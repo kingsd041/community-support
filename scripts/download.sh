bc34799f5acbab80d9d6af1d1f4211a2fdcead88
kingsd041 <nicholas_ksd@hotmail.com>
2021-03-17 10:05:53 +0800

    add rke2

--- scripts/download.sh
+++ scripts/download.sh
@@ -452,6 +452,31 @@ autok3s_channels()
     done
 }
 
+## Rancher RKE2
+rke2_download()
+{
+    repo=rancher/rke2
+
+    version=$( curl -LSs -u $token -s https://api.github.com/repos/$repo/git/refs/tags | jq -r .[].ref | awk -F/ '{print $3}' | grep v | awk -Fv '{print $2}' | grep -v rc[0-9] | grep -v alpha | awk -F"." '{arr[$1"."$2]=$3}END{for(var in arr){if(arr[var]==""){print var}else{print var"."arr[var]}}}' | sort -r  -u -t "." -k1n,1 -k2n,2 -k3n,3 )
+
+    oss_version=$(/usr/local/bin/ossutil --config-file=/root/.ossutilconfig ls oss://$oss_bucket_name/`echo $repo | awk -F/ '{ print $2 }'`/ -d | awk -F "\/" '{print $5}'  | grep v | sed 's/.//' | sort -r  -u -t "." -k1n,1 -k2n,2 -k3n,3)
+
+    compare_version "$version" "$oss_version"
+
+    for ver in $new_version;
+    do
+
+        mkdir -p $download_dir/`echo $repo | awk -F/ '{ print $2 }'`/releases/download/v$ver
+
+        file_name=$(curl -LSs -u $token -s https://api.github.com/repos/$repo/releases/tags/v$ver | jq -r .assets[].browser_download_url | awk -F "/" '{print $NF}' )
+
+        for file in $file_name;
+        do
+            curl -LSs https://github.com/$repo/releases/download/v$ver/$file -o $download_dir/`echo $repo | awk -F/ '{ print $2 }'`/releases/download/v$ver/$file
+        done
+    done
+}
+
 
 output_download_result()
 {
@@ -474,5 +499,6 @@ octopus_download
 harvester_download
 autok3s_download
 autok3s_channels
+rke2_download
 
 output_download_result
